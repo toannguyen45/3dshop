7b87dad760bc8a7405326582a4c68bd173da12fb
toannguyen-valuence <nguyen.minh.toan@valuence.inc>
2024-08-19 17:12:52 +0700

    fix: fix carousel final

---
+++ src/Components/Client/Carousel/CarouselReact.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
+import { Carousel } from 'react-responsive-carousel'
+import './CarouselReact.scss'
+
+const CarouselReact = () => {
+  return (
+    <Carousel
+      autoPlay
+      infiniteLoop
+      stopOnHover
+      showThumbs={false}
+      emulateTouch={true}
+    >
+      <div>
+        <img
+          src="/src/assets/images/client/slide-one.jpg"
+          className="carousel-img"
+          alt="slide-one"
+        />
+        <div className="slider-content">
+          <p className="text-line-one">In 3D sản xuất hàng loạt</p>
+          <p className="text-line-two">Nhanh - Đẹp - Giá tốt</p>
+        </div>
+      </div>
+      <div>
+        <img
+          src="/src/assets/images/client/slide-two.jpg"
+          className="carousel-img"
+          alt="slide-two"
+        />
+        <div className="slider-content">
+          <p className="text-line-one">In 3D sản xuất hàng loạt</p>
+          <p className="text-line-two">Nhanh - Đẹp - Giá tốt</p>
+        </div>
+      </div>
+    </Carousel>
+  )
+}
+
+export default CarouselReact
--- src/Pages/Client/Home/Home.jsx
+++ src/Pages/Client/Home/Home.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import './Home.css'
-import Slider from '../../../Components/Client/Carousel/Slider'
 import AboutUs from '../../../Components/Client/Session/AboutUs/AboutUs'
 import Advise from '../../../Components/Client/Session/Advise/Advise'
 import News from '../../../Components/Client/Session/News/News'
-import { SliderOld } from '../../../Components/Client/Carousel/SliderOld'
+import CarouselReact from '../../../Components/Client/Carousel/CarouselReact'
 
 const Home = () => {
   return (
     <>
-      {/* <Slider autoSlide={true} autoSlideInterval={4000}>
-        <img src='/src/assets/images/client/slide-one.jpg' className='img-slide' />
-        <img src='/src/assets/images/client/slide-two.jpg' className='img-slide' />
-      </Slider> */}
-      {/* <SliderOld/> */}
+      <CarouselReact />
       <AboutUs />
       <News />
       <Advise />
