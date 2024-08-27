79c3b25e2d0e29ae3eb7760c4f23c603a8e2699e
toannguyen-valuence <nguyen.minh.toan@valuence.inc>
2024-08-28 00:15:06 +0700

    fix: fix blogs

--- src/Pages/Client/Blogs/Blogs.jsx
+++ src/Pages/Client/Blogs/Blogs.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Blogs.scss'
+import { Pagination } from 'antd'
 import BreadCrumbCustom from '../../../Components/Client/BreadCrumbCustom/BreadCrumbCustom'
 import MetaSeo from '../../../Components/MetaSeo/MetaSeo'
+import { useDispatch, useSelector } from 'react-redux'
+import { getBlogsClient } from '../../../Features/Blog/BlogSlice'
+import { formatDateTimeFull } from '../../../Utils/formatDate'
+import { storage_url } from '../../../Utils/baseUrl'
+import { Link } from 'react-router-dom'
 import SkeletonBlogs from './SkeletonBlogs'
 
-const BlogsData = React.lazy(
-  () => import('../../../Components/Client/BlogsData/BlogsData')
-)
-
 const Blogs = () => {
+  const dispatch = useDispatch()
+
+  const { blogsClient, isLoading } = useSelector(state => state.blog)
+
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const fetchData = () => {
+    dispatch(getBlogsClient(currentPage))
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [currentPage])
+
   const items = [
     {
       title: 'Tin tức',
@@ -27,9 +43,40 @@ const Blogs = () => {
         <div className="news-column">
           <h2>Tin tức</h2>
           <hr className="divider" />
-          <React.Suspense fallback={<SkeletonBlogs amount={5} />}>
-            <BlogsData />
-          </React.Suspense>
+          {isLoading ? (
+            <SkeletonBlogs amount={3} />
+          ) : (
+            blogsClient?.data?.map((item, index) => (
+              <div key={item.id} className="blogs-item">
+                <img
+                  loading="lazy"
+                  src={`${storage_url}/${item.image}`}
+                  alt={item.title}
+                  className="news-item-image"
+                />
+                <h3 className="news-item-title">{item.title}</h3>
+                <div className="news-item-meta">
+                  <span className="meta-label">Ngày đăng: </span>
+                  <span className="meta-data">
+                    {formatDateTimeFull(item.created_at)}
+                  </span>
+                  <span className="meta-label">Tác giả: </span>
+                  <span className="meta-data">{item.author}</span>
+                </div>
+                <p className="news-item-description">{item.summary}</p>
+                <Link to={`/tin-tuc/${item.slug}`} className="news-item-more">
+                  Xem thêm
+                </Link>
+              </div>
+            ))
+          )}
+          <Pagination
+            className="pagination-custom"
+            current={currentPage}
+            total={blogsClient?.total}
+            pageSize={5}
+            onChange={page => setCurrentPage(page)}
+          />
         </div>
       </div>
     </div>
