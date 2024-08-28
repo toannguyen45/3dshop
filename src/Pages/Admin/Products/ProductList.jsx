98f433798d798317867526ea771a7866a09beb4f
toannguyen-valuence <nguyen.minh.toan@valuence.inc>
2024-08-29 00:23:01 +0700

    fix: fix product list

--- src/Pages/Admin/Products/ProductList.jsx
+++ src/Pages/Admin/Products/ProductList.jsx
@@ -3,38 +3,69 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { Button, Flex, Space, Table } from 'antd'
 import BreadCrumbCus from '../../../Components/Admin/BreadCrumbCus'
-import { getProducts } from '../../../Features/Product/ProductSlice'
+import {
+  deleteProduct,
+  getProducts,
+} from '../../../Features/Product/ProductSlice'
 import CustomModal from '../../../Components/Admin/CustomModal'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import { storage_url } from '../../../Utils/baseUrl'
 
 const ProductList = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const [open, setOpen] = useState(false)
+  const [productId, setProductId] = useState('')
+
+  const { products, isLoading } = useSelector(state => state.product)
+
+  const items = [
+    {
+      title: 'Products',
+    },
+  ]
 
   const [tableParams, setTableParams] = useState({
     pagination: {
       current: 1,
       pageSize: 10,
     },
-    filters: {},
-    sortField: null,
-    sortOrder: null,
   })
 
-  const { products, isLoading } = useSelector(state => state.product)
+  const fetchData = () => {
+    dispatch(getProducts(tableParams))
+  }
 
   useEffect(() => {
-    dispatch(getProducts())
-  }, [])
+    fetchData()
+  }, [
+    tableParams.pagination?.current,
+    tableParams.pagination?.pageSize,
+    tableParams?.sortOrder,
+    tableParams?.sortField,
+    JSON.stringify(tableParams.filters),
+  ])
+
+  useEffect(() => {
+    if (products?.data) {
+      setTableParams(prev => ({
+        ...prev,
+        pagination: {
+          ...prev.pagination,
+          total: products.data.total,
+          current: products.data.current_page,
+        },
+      }))
+    }
+  }, [products])
 
   const handleTableChange = (pagination, filters, sorter) => {
     setTableParams({
       pagination,
       filters,
-      sortField: sorter.field,
-      sortOrder: sorter.order,
+      sortOrder: Array.isArray(sorter) ? undefined : sorter.order,
+      sortField: Array.isArray(sorter) ? undefined : sorter.field,
     })
-    dispatch(getProducts(tableParams))
   }
 
   const handleEdit = record => {
@@ -46,24 +77,12 @@ const ProductList = () => {
 
   const showModal = e => {
     setOpen(true)
-    setCateId(e)
+    setProductId(e)
   }
 
   const hideModal = () => {
     setOpen(false)
   }
-
-  const deleteAProduct = e => {
-    dispatch(deleteProduct(e))
-
-    setOpen(false)
-  }
-
-  const items = [
-    {
-      title: 'Products',
-    },
-  ]
 
   const columns = [
     {
@@ -71,6 +90,23 @@ const ProductList = () => {
       dataIndex: 'id',
       sorter: true,
       width: '20%',
+    },
+    {
+      title: 'Image',
+      dataIndex: 'image',
+      sorter: false,
+      width: '30%',
+      render: (text, record) => {
+        return (
+          <LazyLoadImage
+            src={`${storage_url}/${record.images[0].image}`}
+            alt={record.title}
+            effect="blur"
+            width={'100%'}
+            height={'auto'}
+          />
+        )
+      },
     },
     {
       title: 'Name',
@@ -81,8 +117,11 @@ const ProductList = () => {
     {
       title: 'Category',
       dataIndex: 'category',
-      sorter: true,
+      sorter: false,
       width: '20%',
+      render: (text, record) => {
+        return record.category.name
+      },
     },
     {
       title: 'Price',
@@ -108,6 +147,15 @@ const ProductList = () => {
     },
   ]
 
+  const deleteAProduct = e => {
+    dispatch(deleteProduct(e))
+
+    setOpen(false)
+    setTimeout(() => {
+      dispatch(getProducts())
+    }, 100)
+  }
+
   return (
     <Space direction="vertical" size="large" style={{ display: 'flex' }}>
       <BreadCrumbCus items={items} />
@@ -125,19 +173,19 @@ const ProductList = () => {
           columns={columns}
           rowKey={record => record.id}
           dataSource={products?.data?.data}
-          loading={isLoading}
           pagination={{
             ...tableParams.pagination,
             showSizeChanger: true,
             pageSizeOptions: ['5', '10', '20'],
           }}
+          loading={isLoading}
           onChange={handleTableChange}
         />
         <CustomModal
           hideModal={hideModal}
           open={open}
           performAction={() => {
-            deleteAProduct(cateId)
+            deleteAProduct(productId)
           }}
           title="Are you sure you want to delete this?"
         />
